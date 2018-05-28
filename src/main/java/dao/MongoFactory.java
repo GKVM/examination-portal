526ce3d6993884e7f135d21d3d98681d81b8167b
Gopikrishna V.M <15769003+GKVM@users.noreply.github.com>
2018-05-29 01:17:56 +0530

    Mongodb path from environment.

--- src/main/java/dao/MongoFactory.java
+++ src/main/java/dao/MongoFactory.java
@@ -6,9 +6,9 @@ import config.ExamConfiguration;
 
 public class MongoFactory {
     public final MongoClient build(final ExamConfiguration configuration) {
-        final MongoClientURI mongoClientURI = /*System.getenv("DATABASE").isEmpty() ?*/
-                new MongoClientURI(configuration.getMongoDbUri()) /*:
-                new MongoClientURI(System.getenv("DATABASE"))*/;
+        final MongoClientURI mongoClientURI = System.getenv("DATABASE").isEmpty() ?
+                new MongoClientURI(configuration.getMongoDbUri()) :
+                new MongoClientURI(System.getenv("DATABASE"));
 
         return new MongoClient(mongoClientURI);
     }
